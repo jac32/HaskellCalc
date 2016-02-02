d39aa6778e9560dd2006bbaf6f2eacf22a8d0db4
keananf <keananf@live.com>
2016-02-02 19:14:40 +0000

    Support for negative numbers added

--- Code/Expr.hs
+++ Code/Expr.hs
@@ -4,8 +4,6 @@ import Parsing
 
 type Name = String
 
--- At first, 'Expr' contains only addition and values. You will need to 
--- add other operations, and variables
 data Expr = Add Expr Expr
   | Sub Expr Expr
   | Mult Expr Expr
@@ -25,8 +23,13 @@ eval :: [(Name, Int)] ->      -- Variable name to value mapping
         Maybe Int             -- Result (if no errors such as missing variables)
 eval vars (Val x) = Just x    -- for values, just give the value directly
 
+--retrieve the value corresponding with the name v (if present) in the vars of the state
+--the list comprehension retrieves all name value pairs in vars where the name is equal to v
 eval vars (Var v) = Just (snd (head [(name, val) | (name, val) <- vars, name == [v]]))
 
+
+--performs addition, subtraction, multiplication, and division if 
+--eval vars x and eval vars y return a Just int, and not Nothing
 eval vars (Add x y) =  case (eval vars x, eval vars y) of
   (Just x', Just y') -> Just (x' + y')
   _ -> Nothing
@@ -49,11 +52,15 @@ eval vars (Div x y) = case (eval vars x, eval vars y) of
 digitToInt :: Char -> Int
 digitToInt x = fromEnum x - fromEnum '0'
 
+negDigitToInt :: Char -> Int
+negDigitToInt x = fromEnum '0' - fromEnum x
+
+--top of parse tree
 pCommand :: Parser Command
-pCommand = do t <- letter
+pCommand = do t <- letter --if variable
               char '='
               e <- pExpr
-              return (Set [t] e)
+              return (Set [t] e) --Set t to e, and store in vars in the state
               ||| do e <- pExpr
                      return (Eval e)
 
@@ -67,16 +74,21 @@ pExpr = do t <- pTerm
                      return (Sub t e)
               ||| return t
 
+
 pFactor :: Parser Expr
 pFactor = do d <- digit
              return (Val (digitToInt d))
+             ||| do char '-' --negative numbers
+                    d <- digit
+                    return (Val(negDigitToInt(d)))
              ||| do v <- letter
                     return (Var v)
                     ||| do char '('
                            e <- pExpr
                            char ')'
                            return e
-             
+
+--gets called in pExpr, to indicate precedence for mult and div              
 pTerm :: Parser Expr
 pTerm = do f <- pFactor
            do char '*'
