d332d360b1d6d8bd7268e4551e369df6fe029a06
jac32 <jac32@st-andrews.ac.uk>
2016-02-03 18:39:16 +0000

    User operations interface added

--- Code/REPL.hs
+++ Code/REPL.hs
@@ -12,7 +12,7 @@ initState = State [] 0 []
 
 -- Given a variable name and a value, return a new set of variables with that name and value added.
 -- If it already exists, remove the old value
-updateVars :: Name ->Int -> [(Name,Int)] -> [(Name,Int)]
+updateVars :: Name -> Int -> [(Name,Int)] -> [(Name,Int)]
 updateVars name value set = (name, value) : dropVar name set
 
 -- Return a new set of variables with the given name removed
@@ -35,7 +35,9 @@ toInt (Just x) = x
 
 process :: State -> Command -> IO ()
 process st (Set var e) 
-  = do let st' = addHistory st { vars  = updateVars var (toInt (eval (vars st) e)) (vars st) } (Set var e)
+  = do let st' = addHistory st {
+             vars  = updateVars var (toInt (eval (vars st) e)) (vars st)
+             } (Set var e)
        repl st'
        
 process st (Fetch e)
@@ -61,10 +63,50 @@ repl st = do putStr ("\n" ++ show (numCalcs st) ++ "> ")
 --before any parsing occurs, the input is checked for ':q" 
 handleInput :: State -> String -> IO ()
 handleInput st inp 
-  | (inp /= ":q") = 
+  | head inp /= ':' =
       case parse pCommand inp of
         [(cmd, "")] -> -- Must parse entire input
           process st cmd
         _ -> do putStrLn "Parse error"
                 repl st
-  | otherwise = putStrLn "Bye"
+  | op == 'h' = do printHelp
+                   repl st
+  | op == 'e' = do executeFile st arg
+                   repl st
+  | op == 'q' = putStrLn "Bye!"
+  | otherwise = do putStrLn "Not a recognised command"
+                   repl st
+  where
+    op = head (tail inp)
+    arg = (words inp) !! 1
+    
+
+
+  
+
+  -- | (head inp) /= ':' = 
+  --     case parse pCommand inp of
+  --       [(cmd, "")] -> -- Must parse entire input
+  --         process st cmd
+  --       _ -> do putStrLn "Parse error"
+  --               repl st
+  -- | (head (tail inp)) == 'q' = putStrLn "Bye!"
+  -- | (head (tail inp)) == 'e' = do (executeFile st (tail (tail inp)))
+                                  
+
+  -- | (head (tail inp)) == 'h' = do printHelp
+  --                                 repl st
+
+  -- | otherwise = do putStrLn "Not recognised"
+  --                  repl st
+    
+
+
+executeFile :: State -> String -> IO ()
+executeFile st adr = do contents <- readFile adr
+                        putStrLn contents
+                        
+  
+  
+printHelp :: IO ()
+printHelp = putStrLn "No help text available"
