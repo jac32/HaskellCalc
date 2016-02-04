4e442a3a3cbb1a396915d216f188b2cf74431190
jac32 <jac32@st-andrews.ac.uk>
2016-02-04 00:54:58 +0000

    Haddock commenting of most of REPL.hs

--- Code/REPL.hs
+++ Code/REPL.hs
@@ -3,36 +3,56 @@ module REPL where
 import Expr
 import Parsing
 
+
+{-| Stores the current state of the system
+Tracks command history, variable values and total number of calculations
+-}
 data State = State { vars :: [(Name, Int)],
                      numCalcs :: Int,
                      history :: [Command] }
 
+-- | Initial system state used for initialising the REPL
 initState :: State
 initState = State [] 0 []
 
--- Given a variable name and a value, return a new set of variables with that name and value added.
--- If it already exists, remove the old value
+{-| Updates the variable list with the given name value pair
+Searches the variable list for the given name then adds the
+name value pair. If the name is already in the list then it is
+simply dropped.
+-}
 updateVars :: Name -> Int -> [(Name,Int)] -> [(Name,Int)]
-updateVars name value set = (name, value) : dropVar name set
+updateVars name value vars = (name, value) : dropVar name vars
 
--- Return a new set of variables with the given name removed
+
+-- | Removes the given name and matching value from the list of variables
 dropVar :: Name -> [(Name,Int)] -> [(Name,Int)]
-dropVar name set = [(n,v) | (n,v) <- set, name /= n] 
+dropVar name vars = [(n,v) | (n,v) <- vars, name /= n] 
 
+
+{-| REDUNDANT Fetches the nth command from the state's command history
+The indexing matches the prompt number printed in the repl when the
+command was originally entered. i.e., The first command entered is
+n = 0. This is just the most common use of 'getNthFromList'
+-}
 getNthCommand :: State -> Int -> Command
-getNthCommand st n = getNthFromList (history st) n
-
-getNthFromList :: [a] -> Int -> a
-getNthFromList list pos | pos == 0 = head list
-                        | otherwise = getNthFromList (tail list) (pos - 1)
+getNthCommand st n = (history st) !! n
 
 -- Add a command to the command history in the state
 addHistory :: State -> Command -> State
 addHistory state command = state { numCalcs = (numCalcs state)  + 1,
                                    history = (history state) ++ [command] }
+
+-- | HACK: Converts maybe Ints to ints 
 toInt :: (Maybe Int) -> Int
 toInt (Just x) = x
+toInt Nothing = undefined
 
+{-| Performs the correct action for the entered commmand in a given state
+3 main commands to be processed:
+1. Setting variable - Update variable list and continue REPL with new state
+2. Fetching from history - Evaluate expression to find out which command to fetch and then execute
+3. Evaluate expression - Add the evaluation to the command history, evaluate the expression and print result before continuing with the REPL
+-}
 process :: State -> Command -> IO ()
 process st (Set var e) 
   = do let st' = addHistory st {
@@ -42,25 +62,26 @@ process st (Set var e)
        
 process st (Fetch e)
   = do let st' = st
-       process st (getNthCommand st (toInt (eval (vars st') e)))
+       process st ((history st) !! (toInt (eval (vars st') e)))
            -- st' should include the variable set to the result of evaluating
        
 process st (Eval e) 
   = do let st' = addHistory st (Eval e)
        putStr(show (toInt (eval (vars st') e))) -- Print the result of evaluation
        repl st'
-
--- Read, Eval, Print Loop
--- This reads and parses the input using the pCommand parser, and calls
--- 'process' to process the command.
--- 'process' will call 'repl' when done, so the system loops.
-
+{-|
+Read, Eval, Print Loop
+This reads and parses the input using the pCommand parser, and calls
+'process' to process the command.
+'process' will call 'repl' when done, so the system loops.
+-}
 repl :: State -> IO ()
 repl st = do putStr ("\n" ++ show (numCalcs st) ++ "> ")
              inp <- getLine
              handleInput st inp 
 
---before any parsing occurs, the input is checked for ':q" 
+
+
 handleInput :: State -> String -> IO ()
 handleInput st inp 
   | head inp /= ':' =
@@ -81,32 +102,9 @@ handleInput st inp
     arg = (words inp) !! 1
     
 
-
-  
-
-  -- | (head inp) /= ':' = 
-  --     case parse pCommand inp of
-  --       [(cmd, "")] -> -- Must parse entire input
-  --         process st cmd
-  --       _ -> do putStrLn "Parse error"
-  --               repl st
-  -- | (head (tail inp)) == 'q' = putStrLn "Bye!"
-  -- | (head (tail inp)) == 'e' = do (executeFile st (tail (tail inp)))
-                                  
-
-  -- | (head (tail inp)) == 'h' = do printHelp
-  --                                 repl st
-
-  -- | otherwise = do putStrLn "Not recognised"
-  --                  repl st
-    
-
-
 executeFile :: State -> String -> IO ()
 executeFile st adr = do contents <- readFile adr
                         putStrLn contents
-                        
-  
   
 printHelp :: IO ()
 printHelp = putStrLn "No help text available"
