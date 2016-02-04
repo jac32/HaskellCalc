6c4708ecc4ab9bd001c925f4128bb0a04e27920a
jac32 <jac32@st-andrews.ac.uk>
2016-02-04 21:22:17 +0000

    BST supports name value pairs

--- Code/BST.hs
+++ Code/BST.hs
@@ -1,4 +1,4 @@
-module Bst where 
+module BST where 
 {-| Represents structure of a Binary Search Tree
 Each node is either empty or is a subtree with 2 children
 which are valid trees
@@ -11,51 +11,52 @@ data Tree a = Empty | Tree a (Tree a) (Tree a)
 Does a traversal and compares the value of the node to be inserted against
 the nodes it traverses to find the insertion point
 -}
-insert :: Ord a => a -> Tree a -> Tree a
-insert x Empty = Tree x Empty Empty
-insert x (Tree node l r) = case compare x node of
-  EQ -> undefined
-  GT -> Tree node l (insert x r)
-  LT -> Tree node (insert x l) r
-
-  
+insert :: Ord n => (n,v) -> Tree (n,v) -> Tree (n,v)
+insert (n,v) Empty = Tree (n,v) Empty Empty
+insert (n,v) (Tree node l r) =
+  case compare n (fst node) of
+  EQ -> Tree (n,v) l r
+  LT -> Tree node (insert (n,v) l) r
+  GT -> Tree node l (insert (n,v) r)
+   
 {-| Removes a specified node from the tree 
 Traverses the tree to find the node to be removed
 If there is an empty subtree then the removal is simple
 Otherwise delete is called on the root to be removed
 -}
-remove :: Ord a => a -> Tree a -> Tree a
-remove x Empty = undefined
-remove x (Tree node l r) = case compare x node of
+
+remove :: Ord n => n -> Tree (n,v) -> Tree (n,v)
+remove n Empty = undefined
+remove n (Tree node l r) = case compare n (fst node) of
   EQ -> delete (Tree node l r)
-  GT -> Tree node l (remove x r)
-  LT -> Tree node (remove x l) r
+  LT -> Tree node (remove n l) r
+  GT -> Tree node l (remove n r)
 
 {-| Returns the tree with the root removed
 In the case of two non-empty sub-trees the root is swapped
 with the smallest node in the right sub-tree
 -}
-delete :: Ord a => Tree a -> Tree a
-delete (Tree a Empty sub) = sub
-delete (Tree a sub Empty) = sub
-delete (Tree a sub1 sub2)
-  = Tree small sub1 (remove small sub2)
-  where small = smallestNode sub2
-
+delete :: Ord n => Tree (n,v) -> Tree (n,v)
+delete (Tree node l Empty) = l
+delete (Tree node Empty r) = r
+delete (Tree small l r)
+  = Tree small l (remove (fst small) r)
+  where small = smallestNode r
+        
 {-| Returns the smallest node in the given tree
 This is useful for deleting roots like in 'delete'
 -}
-smallestNode :: Ord a => Tree a -> a
-smallestNode (Tree a Empty _) = a
-smallestNode (Tree _ sub _) = smallestNode sub
+smallestNode :: Tree (n,v) -> (n,v)
+smallestNode (Tree node Empty _) =  node
+smallestNode (Tree _ l _) = smallestNode l
 
 {-| Checks a tree for the presence of the given value
 True if tree contains a node of the given value
 otherwise False
 -}
-contains :: Ord a => a -> Tree a -> Bool
-contains x Empty = False
-contains x (Tree node l r) = case compare x node of
+contains :: Ord n => n -> Tree (n,v) -> Bool
+contains n Empty = False
+contains n (Tree node l r) = case compare n (fst node) of
   EQ -> True
-  GT -> (contains x r)
-  LT -> (contains x l)
+  LT -> (contains n l)
+  GT -> (contains n r)
