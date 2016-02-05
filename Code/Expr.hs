406aa194ee04b66e006d1f3ddf14a9256a4c2572
keananf <keananf@live.com>
2016-02-05 16:31:54 +0000

    Separated the parse tree and the eval functions. The Parse Tree is in Command.hs and the eval functions remain in Expr.hs

--- Code/Expr.hs
+++ Code/Expr.hs
@@ -1,6 +1,5 @@
 module Expr where
 
-import Parsing
 import Value
 import BST
 
@@ -91,88 +90,5 @@ eval vars (Or x y) = case (eval vars x, eval vars y) of
   _ -> Nothing
 
   
---top of parse tree
-pCommand :: Parser Command
-pCommand = do character '#'
-              e <- pExpr
-              return (Fetch e)
-              ||| do t <- identifier --if variable
-                     character '='
-                     e <- pExpr
-                     return (Set t e) --Set t to e, and store in vars in the state
-                     ||| do e <- pExpr
-                            return (Eval e)
 
-pExpr :: Parser Expr
-pExpr = do b <- pLogExpr
-           return b
-           ||| do t <- pTerm
-                  do character '+'
-                     e <- pExpr
-                     return (Add t e)
-                     ||| do character '-'
-                            e <- pExpr
-                            return (Sub t e)
-                     ||| return t
-
-
-pBool :: Parser Expr
-pBool = do symbol "!"
-           b <- pBool
-           return (Not b)
-           ||| do b <- bool
-                  return (Val (B b))
-           ||| do i <- identifier
-                  return (Var i)
-           ||| do character '('
-                  b <- pLogExpr
-                  character ')'
-                  return b
-           
-pLogExpr :: Parser Expr
-pLogExpr = do b1 <- pBool
-              do symbol "&&"
-                 b2 <- pLogExpr
-                 return (And b1 b2)
-                 ||| do symbol "||"
-                        b2 <- pLogExpr
-                        return (Or b1 b2)
-                        
-                 ||| return b1
-           
-
-pFactor :: Parser Expr
-pFactor = do d <- float
-             return (Val  (F d))
-             ||| do d <- integer
-                    return (Val (I d))
-             ||| do character '-'
-                    e <- pFactor --negative expressions
-                    return (Neg (e))
-             ||| do v <- identifier
-                    return (Var v)
-                    ||| do character '('
-                           e <- pExpr
-                           character ')'
-                           return e
-                    ||| do character '|'
-                           e <- pExpr
-                           character '|'
-                           return (Abs e)
---gets called in pExpr, to indicate precedence for mult and div               
-pTerm :: Parser Expr 
-pTerm = do f <- pFactor
-           do character '^'
-              t <- pExpr
-              return (Pow f t)
-              ||| do character '*'
-                     t <- pTerm
-                     return (Mult f t)
-              ||| do character '/'
-                     t <- pTerm
-                     return (Div f t)
-              ||| do character '%'
-                     t <- pTerm
-                     return (Mod f t)
-              ||| return f
 
