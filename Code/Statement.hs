2cf7f5e5c8a8ac8ec84626285f4509c0ceb73778
jac32 <jac32@st-andrews.ac.uk>
2016-02-06 22:38:16 +0000

    Proposed parse structure replacement

---
+++ Code/Statement.hs
@@ -0,0 +1,141 @@
+module Statement where
+
+import Parsing
+import Value
+
+type Name = String
+
+data Stmt = Eval Expr
+  | ASet  Name  AExpr
+  | BSet  Name  BExpr
+  | If    BExpr Stmt
+  | While BExpr Stmt
+  deriving Show
+
+data Expr = AE AExpr | BE BExpr deriving Show
+data BExpr = Const Bool
+  | Not BExpr
+  | BVar Name
+  | Or  BExpr BExpr
+  | And BExpr BExpr
+  | Eq  AExpr AExpr
+  | Gt  AExpr AExpr
+  | Lt  AExpr AExpr
+  deriving Show
+
+data AExpr = Var Name
+  | Val Value
+  | Neg AExpr
+  | Abs AExpr
+  | AOp AExpr AExpr
+  | Add AExpr AExpr
+  | Sub AExpr AExpr
+  | Mul AExpr AExpr
+  | Div AExpr AExpr
+  | Mod AExpr AExpr
+  | Pow AExpr AExpr
+    deriving Show
+
+
+pStmt :: Parser Stmt
+pStmt = do symbol "if"
+           b <- pBExpr
+           s <- pStmt
+           return (If b s)
+           ||| do symbol "while"
+                  b <- pBExpr
+                  s <- pStmt
+                  return (While b s)
+           ||| do n <- identifier
+                  symbol "="
+                  v <- pAExpr
+                  return (ASet n v)
+           ||| do n <- identifier
+                  symbol "="
+                  v <- pBExpr
+                  return (BSet n v)
+           ||| do e <- pBExpr
+                  return (Eval (BE e))
+           ||| do e <- pAExpr
+                  return (Eval (AE e))
+                  
+                 
+-- Code here could be simplified
+-- NOT needs implemented
+pBExpr :: Parser BExpr
+pBExpr = do b1 <- pBTerm
+            do symbol "&&"
+               b2 <- pBExpr
+               return (And b1 b2)
+               ||| do symbol "||"
+                      b2 <- pBExpr
+                      return (Or b1 b2)
+               ||| return b1
+          
+                  
+            
+pBTerm :: Parser BExpr
+pBTerm = do b <- bool
+            return (Const b)
+            ||| do symbol "("
+                   e <- pBExpr
+                   symbol ")"
+                   return e
+            ||| do v1 <- pAExpr
+                   do symbol "<"
+                      v2 <- pAExpr
+                      return (Lt v1 v2)
+                      ||| do symbol "=="
+                             v2 <- pAExpr
+                             return (Eq v1 v2)
+                      ||| do symbol ">"
+                             v2 <- pAExpr
+                             return (Gt v1 v2)
+            -- ||| do i <- identifier
+            --        return (BVar i)
+
+
+               
+pAExpr :: Parser AExpr
+pAExpr = do t <- pATerm
+            do symbol "+"
+               e <- pAExpr
+               return (Add t e)
+               ||| do symbol "-"
+                      e <- pAExpr
+                      return (Sub t e)
+               ||| return t
+
+pATerm :: Parser AExpr
+pATerm = do f <- pFactor 
+            do symbol "*"
+               t <- pATerm
+               return (Mul f t)
+               ||| do symbol "/"
+                      t <- pATerm
+                      return (Div f t)
+               ||| do symbol "%"
+                      t <- pATerm
+                      return (Mod f t)
+               ||| return f
+              
+pFactor :: Parser AExpr
+pFactor = do f <- float
+             return  (Val (F f))
+             ||| do i <- integer
+                    return (Val (I i))
+             ||| do symbol "-"
+                    e <- pFactor
+                    return (Neg e)
+             ||| do v <- identifier
+                    return (Var v)
+             ||| do symbol "("
+                    e <- pAExpr
+                    symbol ")"
+                    return e
+             ||| do symbol "|"
+                    e <- pAExpr
+                    symbol "|"
+                    return (Abs e)                 
+
+             
